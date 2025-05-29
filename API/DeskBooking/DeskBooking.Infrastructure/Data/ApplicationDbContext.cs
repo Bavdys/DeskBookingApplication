bed8f2f4abc71adbc12c58536d8aac9dacda6047
Bavdys Vladyslav <bavdis1996@gmail.com>
2025-05-29 14:05:22 +0300

    Add ApplicationDbContext class to DeskBooking.Infrastructure project

---
+++ API/DeskBooking/DeskBooking.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DeskBooking.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeskBooking.Infrastructure.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<BookingType> BookingTypes { get; set; }
+        public DbSet<Capacity> Capacities { get; set; }
+        public DbSet<Picture> Pictures { get; set; }
+        public DbSet<Workspace> Workspaces { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
