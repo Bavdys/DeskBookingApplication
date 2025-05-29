66cb957f4e5c730e69a6259bdaed6f806306dbaf
Bavdys Vladyslav <bavdis1996@gmail.com>
2025-05-29 14:26:49 +0300

    Add ServiceCollectionExtension class in DeskBooking.API project

---
+++ API/DeskBooking/DeskBooking/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using DeskBooking.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeskBooking.API.Extensions
+{
+    internal static class ServiceCollectionExtension
+    {
+        internal static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
