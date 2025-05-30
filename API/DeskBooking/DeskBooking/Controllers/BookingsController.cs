ac7032c10e40e1b35dd79c9a03220480a0cd6492
Bavdys Vladyslav <bavdis1996@gmail.com>
2025-05-30 19:36:54 +0300

    Add controllers

---
+++ API/DeskBooking/DeskBooking/Controllers/BookingsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DeskBooking.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookingsController : ControllerBase
+    {
+        public BookingsController() { }
+
+        [HttpGet]
+        public async Task<ActionResult> GetBookings() /////TODO
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetBooking() /////TODO
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateBooking() /////TODO
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateBooking() /////TODO
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteBooking() /////TODO
+        {
+            return Ok();
+        }
+    }
+}
---
+++ API/DeskBooking/DeskBooking/Controllers/WorkspacesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DeskBooking.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkspacesController : ControllerBase
+    {
+        public WorkspacesController() { }
+
+        [HttpGet]
+        public async Task<ActionResult> GetWorkspaces() /////TODO
+        {
+            return Ok();
+        }
+    }
+}
